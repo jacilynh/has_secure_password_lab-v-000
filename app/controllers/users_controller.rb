b0600eb841bddc68091e35101bee4aa1d0f1e745
Jacilyn Hayden <jacihayden@gmail.com>
2018-08-25 18:50:49 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
       def user_params
           params.require(:user).permit(:name, :password, :password_confirmation)
       end
+      
       def index
       end
 end
